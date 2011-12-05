811396310994fc434befdeefd9f8c908ae138181
Dallon Feldner <dallonfdev@gmail.com>
2011-12-05 12:49:04 -0700

    Initial Commit

---
+++ iNdecsisive/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iNdecsisive
+//
+//  Created by Dallon Feldner on 12/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iNdecsisive/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  iNdecsisive
+//
+//  Created by Dallon Feldner on 12/5/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
